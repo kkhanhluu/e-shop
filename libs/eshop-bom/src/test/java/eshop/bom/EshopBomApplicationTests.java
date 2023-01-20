4085f1d4b0d01d1f61b4bdd147933bba88584b25
Khanh Luu <luukhanh96@gmail.com>
2023-01-20 01:09:24 +0100

    # This is a combination of 2 commits. # This is the 1st commit message:
    feat: setup bom dependencies and plugins
    
    # This is the commit message #2:
    
    test: test semantic pipeline

---
+++ libs/eshop-bom/src/test/java/eshop/bom/EshopBomApplicationTests.java
@@ -0,0 +1,13 @@
+package eshop.bom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopBomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
