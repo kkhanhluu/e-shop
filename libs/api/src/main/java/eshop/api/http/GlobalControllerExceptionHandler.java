1aecb92c0404299e977f3cf0a3268d23275037f8
Khanh Luu <luukhanh96@gmail.com>
2023-01-23 00:39:46 +0100

    fix: fix global controller exception handler

--- libs/api/src/main/java/eshop/api/http/GlobalControllerExceptionHandler.java
+++ libs/api/src/main/java/eshop/api/http/GlobalControllerExceptionHandler.java
@@ -2,10 +2,10 @@ package eshop.api.http;
 
 import eshop.api.exceptions.BadRequestException;
 import eshop.api.exceptions.NotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -17,18 +17,18 @@ public class GlobalControllerExceptionHandler {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(BadRequestException.class)
-    public @ResponseBody HttpErrorInfo handleBadRequestExceptions(ServerHttpRequest request, BadRequestException ex) {
+    public @ResponseBody HttpErrorInfo handleBadRequestExceptions(HttpServletRequest request, BadRequestException ex) {
         return createHttpErrorInfo(HttpStatus.BAD_REQUEST, request, ex);
     }
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NotFoundException.class)
-    public @ResponseBody HttpErrorInfo handleNotFoundExceptions(ServerHttpRequest request, NotFoundException ex) {
+    public @ResponseBody HttpErrorInfo handleNotFoundExceptions(HttpServletRequest request, NotFoundException ex) {
         return createHttpErrorInfo(HttpStatus.NOT_FOUND, request, ex);
     }
 
-    private HttpErrorInfo createHttpErrorInfo(HttpStatus httpStatus, ServerHttpRequest request, Exception ex) {
-        final String path = request.getPath().pathWithinApplication().value();
+    private HttpErrorInfo createHttpErrorInfo(HttpStatus httpStatus, HttpServletRequest request, Exception ex) {
+        final String path = request.getRequestURI();
         final String message = ex.getMessage();
 
         LOG.debug("Returning HTTP status: {} for paht {}, message: {}", httpStatus, path, message);
