a6a0558b95864cc06d438cb46388b8743602807b
Khanh Luu <luukhanh96@gmail.com>
2023-02-09 13:00:20 +0100

    fix: fix user service test

--- apps/user-service/src/test/java/eshop/userservice/UserServiceApplicationTests.java
+++ apps/user-service/src/test/java/eshop/userservice/UserServiceApplicationTests.java
@@ -1,10 +1,32 @@
 package eshop.userservice;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
+@Testcontainers
 class UserServiceApplicationTests {
+	@Container
+	static PostgreSQLContainer postgresContainer = new PostgreSQLContainer(DockerImageName.parse("postgres:15.1"));
+
+	@DynamicPropertySource
+	static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
+		dynamicPropertyRegistry.add("spring.datasource.url", postgresContainer::getJdbcUrl);
+		dynamicPropertyRegistry.add("spring.datasource.username", postgresContainer::getUsername);
+		dynamicPropertyRegistry.add("spring.datasource.password", postgresContainer::getPassword);
+	}
+
+	@BeforeAll
+	static void beforeAll() {
+		postgresContainer.start();
+	}
 
 	@Test
 	void contextLoads() {
