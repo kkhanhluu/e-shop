cc90cdce0933a3b54c552a89fc994e8148bb6c49
Khanh Luu <luukhanh96@gmail.com>
2023-03-16 03:00:10 +0100

    feat: add seed script for reviews

--- tools/seed-db.js
+++ tools/seed-db.js
@@ -1,5 +1,6 @@
 const { Client } = require('pg');
 const { faker } = require('@faker-js/faker');
+const { MongoClient } = require('mongodb');
 
 const CONFIG = {
   host: 'localhost',
@@ -19,11 +20,15 @@ const inventoryClient = new Client({
   ...CONFIG,
   database: 'inventory_service',
 });
+const reviewClient = new MongoClient(
+  'mongodb://mongo:mongo@localhost:27017?authMechanism=DEFAULT'
+);
 
 async function main() {
   await productClient.connect();
   await inventoryClient.connect();
   await userClient.connect();
+  await reviewClient.connect();
 
   const products = (
     await Promise.all(
@@ -53,7 +58,28 @@ async function main() {
       })
     )
   );
+
+  const reviewDb = reviewClient.db('review-service');
+  const reviewCollection = reviewDb.collection('reviews');
+  await reviewCollection.insertMany(
+    products
+      .map((product) =>
+        new Array(10).fill(null).map(() => ({
+          productId: product.id,
+          text: faker.lorem.paragraph(5),
+          rate: faker.datatype.number({ min: 1, max: 5, precision: 1 }),
+          createdAt: faker.date.past(),
+        }))
+      )
+      .flat()
+  );
 }
 main()
   .then(() => console.log('Done'))
-  .catch((e) => console.log('Error', e));
+  .catch((e) => console.log('Error', e))
+  .finally(async () => {
+    await productClient.end();
+    await userClient.end();
+    await inventoryClient.end();
+    await reviewClient.close();
+  });
