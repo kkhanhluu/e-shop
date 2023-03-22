e16a2ea7f910f569a15870348de27a256d6402a4
Khanh Luu <luukhanh96@gmail.com>
2023-03-22 17:52:27 +0100

    feat: update proto file for product service

---
+++ apps/api-gateway/src/product/handlers/get.ts
@@ -0,0 +1,29 @@
+import { z } from 'zod';
+import {
+  GetInventoryByProductIdRequest,
+  Inventory,
+} from '../../../generated/proto/InventoryService';
+import { inventoryServiceClient } from '../clients/grpc';
+
+export const GetInventoryByProductIdInput = z.object({
+  productId: z.string().uuid(),
+});
+
+export function getInventoryByProductId(
+  input: z.infer<typeof GetInventoryByProductIdInput>
+): Promise<Inventory> {
+  return new Promise((resolve, reject) => {
+    const request = GetInventoryByProductIdRequest.fromJSON(input);
+    inventoryServiceClient.getInventoryByProductId(
+      request,
+      (error, response) => {
+        if (error) {
+          console.error(error);
+          reject(error);
+        } else {
+          resolve(response);
+        }
+      }
+    );
+  });
+}
