6e9ef8789fb14b97cd0cf1fa7766a360d7cde522
Khanh Luu <luukhanh96@gmail.com>
2023-03-29 02:58:16 +0200

    chore: add k8s for order service

--- k8s/templates/deploy-apps.sh
+++ k8s/templates/deploy-apps.sh
@@ -9,3 +9,9 @@ kubectl apply -f ./k8s/templates/mongo/service.yaml
 
 kubectl apply -f ./k8s/templates/eventstore/deployment.yaml
 kubectl apply -f ./k8s/templates/eventstore/service.yaml
+
+kubectl apply -f ./k8s/templates/rabbitmq/deployment.yaml
+kubectl apply -f ./k8s/templates/rabbitmq/service.yaml
+
+kubectl apply -f ./k8s/templates/order-service/service.yaml
+kubectl apply -f ./k8s/templates/order-service/deployment.yaml
