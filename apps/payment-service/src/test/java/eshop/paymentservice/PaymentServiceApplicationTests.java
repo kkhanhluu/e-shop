4f55f479a00f0b7f4329becd4dfe9d61ea614ed9
Khanh Luu <luukhanh96@gmail.com>
2023-03-30 00:33:54 +0200

    fix: fix test for payment service

--- apps/payment-service/src/test/java/eshop/paymentservice/PaymentServiceApplicationTests.java
+++ apps/payment-service/src/test/java/eshop/paymentservice/PaymentServiceApplicationTests.java
@@ -5,29 +5,39 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.containers.RabbitMQContainer;
 import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
+@Testcontainers
 class PaymentServiceApplicationTests {
 
-	@Container
-	static RabbitMQContainer rabbitMQContainer = new RabbitMQContainer(DockerImageName.parse("rabbitmq:3.11.10"));
+    @Container
+    static RabbitMQContainer rabbitMQContainer = new RabbitMQContainer(
+            DockerImageName.parse("rabbitmq:3.11.10-management-alpine")).withVhost("/")
+            .withQueue("q.compensate-payment");
 
-	@DynamicPropertySource
-	static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
-		dynamicPropertyRegistry.add("spring.rabbitmq.host", rabbitMQContainer::getHost);
-		dynamicPropertyRegistry.add("spring.rabbitmq.port", rabbitMQContainer::getAmqpPort);
-	}
+    @Container
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer(DockerImageName.parse("mongo:6.0.3"));
 
-	@BeforeAll
-	static void beforeAll() {
-		rabbitMQContainer.start();
-	}
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
+        dynamicPropertyRegistry.add("spring.rabbitmq.host", rabbitMQContainer::getHost);
+        dynamicPropertyRegistry.add("spring.rabbitmq.port", rabbitMQContainer::getAmqpPort);
 
-	@Test
-	void contextLoads() {
-	}
+        dynamicPropertyRegistry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
 
+    @BeforeAll
+    static void beforeAll() {
+        rabbitMQContainer.start();
+        mongoDBContainer.start();
+    }
+
+    @Test
+    void contextLoads() {
+    }
 }
