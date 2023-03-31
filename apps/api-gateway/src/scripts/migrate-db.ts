a1f9a1d9009be8f9b0ec4787f6b62dc53db798e7
Khanh Luu <luukhanh96@gmail.com>
2023-03-31 17:15:32 +0200

    fix: fix db migration

--- apps/api-gateway/src/scripts/migrate-db.ts
+++ apps/api-gateway/src/scripts/migrate-db.ts
@@ -1,10 +1,18 @@
 import { execSync } from 'node:child_process';
 
 function migrateDatabase(database: string) {
-  execSync(
-    `export DATABASE_URL=postgresql://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${database}_service && npx prisma migrate deploy --schema ./apps/api-gateway/src/${database}/prisma/schema.prisma`,
-    { stdio: 'inherit' }
-  );
+  console.log(process.env.NODE_ENV);
+  if (process.env.NODE_ENV === 'development') {
+    execSync(
+      `export DATABASE_URL=postgresql://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${database}_service && npx prisma migrate deploy --schema ./apps/api-gateway/src/${database}/prisma/schema.prisma`,
+      { stdio: 'inherit' }
+    );
+  } else {
+    execSync(
+      `export DATABASE_URL=postgresql://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${database}_service && npx prisma migrate deploy --schema ./api-gateway/${database}/prisma/schema.prisma`,
+      { stdio: 'inherit' }
+    );
+  }
 }
 
 export function migrateDatabases() {
