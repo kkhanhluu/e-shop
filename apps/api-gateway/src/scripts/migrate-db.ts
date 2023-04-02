16e2bcf60edf5eebd76466565f38577624517801
Khanh Luu <luukhanh96@gmail.com>
2023-04-02 14:26:40 +0200

    fix: remove console.log

--- apps/api-gateway/src/scripts/migrate-db.ts
+++ apps/api-gateway/src/scripts/migrate-db.ts
@@ -1,7 +1,6 @@
 import { execSync } from 'node:child_process';
 
 function migrateDatabase(database: string) {
-  console.log(process.env.NODE_ENV);
   if (process.env.NODE_ENV === 'development') {
     execSync(
       `export DATABASE_URL=postgresql://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${database}_service && npx prisma migrate deploy --schema ./apps/api-gateway/src/${database}/prisma/schema.prisma`,
