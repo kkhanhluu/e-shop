94c15d2fe8465ff871e8ed479ef72d6c1b4ce983
Khanh Luu <luukhanh96@gmail.com>
2023-04-07 20:58:23 +0200

    chore: fix broken link

--- apps/docs/docusaurus.config.js
+++ apps/docs/docusaurus.config.js
@@ -21,7 +21,7 @@ const config = {
   organizationName: 'kkhanhluu', // Usually your GitHub org/user name.
   projectName: 'e-shop', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internalization, you can use this field to set useful
